bb6ba3006dd36f94bc05238bf9c12e47d97f2114
RobertJacobs1102 <54903882+RobertJacobs1102@users.noreply.github.com>
2024-05-26 17:45:37 -0400

    Update AboutComponent.js

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -16,17 +16,14 @@ const AboutComponent = () => {
               </div>
             </div>
             <div className="desc">
+              <p>Syntale is a popular Unturned server network with many unique features.</p>
               <p>
-                Syntale is a popular Unturned server network with many unique
-                features.
+                Thanks to our incredible community and diligent staff team, we are able to offer an
+                amazing experience to everyone.
               </p>
               <p>
-                Thanks to our incredible community and diligent staff team, we
-                are able to offer an amazing experience to everyone.
-              </p>
-              <p>
-                Our servers range from pure vanilla survival servers, heavily
-                modded semi-vanilla servers, to amazing full role play servers.
+                Our servers range from pure vanilla survival servers, heavily modded semi-vanilla
+                servers, to amazing full role play servers.
               </p>
             </div>
             <div className="buttons">
@@ -48,7 +45,13 @@ const AboutComponent = () => {
         {/* About Item #2 */}
         <div className="neoh_fn_about_item reverse">
           <div className="img_item">
-            <img src="https://ready2frag.com/favicon/favicon-og.png" style={{width: 600, height: 600}} alt="Ready2Frag Favicon" width={1000} height={1000}/>
+            <img
+              src="https://hostvenom.com/images/servers.png"
+              style={{ width: 600, height: 600 }}
+              alt="Ready2Frag Favicon"
+              width={1000}
+              height={1000}
+            />
           </div>
           <div className="content_item">
             <div className="neoh_fn_title" data-align="left">
@@ -59,18 +62,21 @@ const AboutComponent = () => {
             </div>
             <div className="desc">
               <p>
-                Syntale&apos;s dedicated servers are equipped with the latest
-                generation 16 core AMD Ryzen CPUs in order to provide the best
-                possible experience to our players.
+                Syntale&apos;s dedicated servers are equipped with the latest generation 16 core AMD
+                Ryzen CPUs in order to provide the best possible experience to our players.
               </p>
               <p>
-                We have dedicated technicians to resolve issues with the servers
-                as fast as possible. Our goal is to provide yosu with the best
-                possible Unturned experience.
+                We have dedicated technicians to resolve issues with the servers as fast as
+                possible. Our goal is to provide yosu with the best possible Unturned experience.
               </p>
             </div>
             <div className="buttons">
-              <a href="https://ready2frag.com/" target="_blank" rel="noreferrer" className="neoh_fn_button only_text">
+              <a
+                href="https://ready2frag.com/"
+                target="_blank"
+                rel="noreferrer"
+                className="neoh_fn_button only_text"
+              >
                 <span className="text">Learn More</span>
               </a>
             </div>
